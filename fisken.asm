2bba853f0f3233021f2156ba48b218b558b62d6b
Vegar Westerlund <vegarwe@gmail.com>
2017-05-17 16:53:11 +0200

    Working

---
+++ fisken.asm
@@ -0,0 +1,156 @@
+;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+;@ @brief Learning ARM assembler
+;@
+;@ Header section
+;@
+    ;PRESERVE8
+    AREA asm_func, CODE, READONLY
+    EXPORT my_asm
+    EXPORT ll_init
+    EXPORT ll_add
+    EXPORT ll_next
+    EXPORT ll_free
+    IMPORT malloc
+    IMPORT free
+
+;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+;@ @brief Random (unexported) assembly function
+my_other_asm
+    ; Do some memory storing
+    mov     r2, #0x0000ffff
+    add     r2, #0x00ee0000
+    add     r2, #0x23000000
+    str     r2, [r1]
+
+    add     r1, r1, #4
+    mov     r2, #0x00001111
+    add     r2, #0x00220000
+    add     r2, #0x44000000
+    str     r2, [r1]
+
+    add     r1, r1, #4
+    mov     r2, #0x00004ee7
+    add     r2, #0x000b0000
+    add     r2, #0xb0000000
+    str     r2, [r1]
+
+    bx      lr
+
+
+;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+;@ @brief Random assembly function
+;@
+;@ Calls my other random assembly function as well as looping
+;@
+;@ @param[in]   value   A number that will be manipulated and returned
+;@ @param[out]  buff    uint32_t array (will be slightly populated)
+;@
+;@ @return      The input value (after beeing manipulated)
+my_asm
+    push    {r0-r3, r12, lr}
+    bl      my_other_asm
+    pop     {r0-r3, r12, lr}
+
+    ; Simple for-loop
+    mov     r0, #0
+for_start
+    ; ...
+    add     r0, #1
+    cmp     r0, #14
+    bne     for_start
+
+    ; Simple while-true loop
+    mov     r12, #19
+while_start
+    add     r0, #1
+    subs    r12, #1
+    bne     while_start
+
+    add     r0, r0, r0, lsl #1      ; r0 = 3 * r0
+    bx      lr
+
+
+;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+;@ @brief Allocate and initialize linked list structure
+;@
+;@ @param[out]  p_ll    Pointer to linked list structure (will be populated)
+;@ @param[in]   value   Value to be stored in new LL structure
+;@
+;@ @return      New linked list node
+ll_init
+    ; Allocate memory
+    push    {r0-r1, r12, lr}
+    mov     r0, #8
+    bl.w    malloc
+    pop     {r2-r3, r12, lr}
+
+    ; Store pointer to new memory in input param
+    str     r0, [r2]
+
+    ; Zero initialize new node
+    ; TODO: store multiple ? memset?
+    mov     r2, #0
+    str     r2, [r0, #0]
+    str     r3, [r0, #4]
+
+    bx      lr
+
+
+;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+;@ @brief Push value (allocating new node for it) into linked list
+;@
+;@ @param[in]   ll      Linked list structure
+;@ @param[in]   value   Value to be stored in new LL structure
+;@
+;@ @return      New linked list node
+ll_add
+    push    {r0-r1, r12, lr}    ; Store registers (including params)
+    mov     r0, #8              ; Need 8 bytes of data for new node
+    bl.w    malloc              ; Allocate memory for new node
+    pop     {r2-r3, r12, lr}    ; Pop params ll and value into r2 and r3
+    ldr     r1, [r2]            ; Fetch current next (if any)
+    str     r0, [r2]            ; Store pointer to new memory in input param
+    str     r1, [r0, #0]        ; Store old next in new node
+    str     r3, [r0, #4]        ; Store value of new node
+    bx      lr
+
+
+;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+;@ @brief Return next linked list entry
+;@
+;@ @param[in]   ll      Linked list structure
+;@
+;@ @return      Next linked list node
+ll_next
+    ldr     r0, [r0]
+    bx      lr
+
+
+;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+;@ @brief Free linked list structure (from input to end)
+;@
+;@ Loop through linked list, freeing nodes
+;@
+;@ @param[in]   ll      (Head of) linked list structure
+;@
+;@ @return      Number of freed elements
+ll_free
+    mov     r2, #0              ; r2 holds freed count
+    mov     r1, r0              ; r1 holds current node
+free_loop
+    cmp     r1, #0              ; Check if next is NULL
+    moveq   r0, r2              ;   Return freed count
+    bxeq    lr                  ;   Return
+    mov     r0, r1              ; Setup node to be freed
+    ldr     r1, [r1]            ; Read in 'next'
+    push    {r1-r2, r12, lr}    ; Store registers
+    bl.w    free                ; Free current
+    pop     {r1-r2, r12, lr}    ; Restore registers
+    add     r2, #1              ; Increment freed count
+    b       free_loop           ; Loop
+
+
+;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+;@ vim:ft=armv5
+
+    end
---
+++ unit_test.c
@@ -0,0 +1,120 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unity.h>
+
+
+// Output on different platforms
+#if defined ( __CC_ARM )
+#include "nrf.h"
+#include "core_cm4.h"
+int fputc(int c, FILE *f) {
+  return(ITM_SendChar(c));
+}
+#endif // __CC_ARM
+
+
+// Tests
+void setUp(void)
+{
+}
+
+void tearDown(void) { }
+
+int my_asm(int, uint32_t*);
+
+void test_fisk_1(void)
+{
+    int k = 7;
+    uint32_t data[7] = {0};
+    k = my_asm(4, data);
+    printf("hei %d\r\n", k);
+
+    printf("data \r\n");
+    for (int i = 0; i < sizeof(data)/sizeof(data[0]); i++) {
+        printf(" %08x\r\n", data[i]);
+    }
+
+}
+
+void create_buffer(char** pp_buf)
+{
+    *pp_buf = malloc(5);
+
+    (*pp_buf)[0] = 'a';
+    (*pp_buf)[1] = 'b';
+    (*pp_buf)[2] = 'c';
+    (*pp_buf)[3] = 'g';
+    (*pp_buf)[4] = '\0';
+}
+
+void free_buffer(char* p_buf)
+{
+    free(p_buf);
+}
+
+void test_fisk_malloc(void)
+{
+    char* buff1;
+    char* buff2;
+    char* buff3;
+
+    create_buffer(&buff1);
+    create_buffer(&buff2);
+    printf("buff1: %p %s\n", buff1, buff1);
+    printf("buff2: %p %s\n", buff2, buff2);
+    free_buffer(buff1);
+    free_buffer(buff2);
+
+    create_buffer(&buff3);
+    printf("buff3: %p %s\n", buff3, buff3);
+    free_buffer(buff3);
+
+}
+
+void*   ll_init(void** ll, uint32_t value);
+void*   ll_add( void*  ll, uint32_t value);
+void*   ll_next(void*  ll);
+int     ll_free(void*  ll);
+
+void test_fisk_ll(void)
+{
+    void* head = NULL;
+    void* ll = ll_init(&head, 1);
+
+    //printf("head: %p %p %p %d\n", &head, head, (void*)*(((uint32_t*)head)+0), *(((uint32_t*)head)+1));
+    //printf("ll  : %p %p %p %d\n", &ll, ll, (void*)*(((uint32_t*)ll)+0), *(((uint32_t*)ll)+1));
+    //ll_add(ll, 0x03);
+    //printf("ll  : %p %p %p %d\n", &ll, ll, (void*)*(((uint32_t*)ll)+0), *(((uint32_t*)ll)+1));
+    //ll = (void*)*((uint32_t*)ll);
+    //printf("ll  : %p %p %p %d\n", &ll, ll, (void*)*(((uint32_t*)ll)+0), *(((uint32_t*)ll)+1));
+
+    ll = ll_add(ll, 2);
+    ll = ll_add(ll, 3);
+    ll = ll_add(ll, 4);
+    ll = ll_add(ll, 5);
+    ll_add(head,    6);
+    ll = ll_add(ll, 7);
+
+    printf("Iterate\n");
+    ll = head;
+    do {
+        printf("ll  : %p %p %p %d\n", &ll, ll, (void*)*(((uint32_t*)ll)+0), *(((uint32_t*)ll)+1));
+    } while ((ll = ll_next(ll)) != NULL);
+
+    int count = ll_free(head);
+    printf("head freed %d\n", count);
+
+    count = ll_free(NULL);
+    printf("NULL freed %d\n", count);
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_fisk_1);
+    RUN_TEST(test_fisk_malloc);
+    RUN_TEST(test_fisk_ll);
+    int result = UNITY_END();
+
+    return result;
+}
